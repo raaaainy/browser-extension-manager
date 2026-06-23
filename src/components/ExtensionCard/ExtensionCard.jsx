c9179f8696c5aee861f906341fb96017d882ed81
Clarissa Barr <clarissab8788@gmail.com>
2026-06-23 10:57:13 -0700

    Fixed Storybook Configuration

--- src/components/ExtensionCard/ExtensionCard.jsx
+++ src/components/ExtensionCard/ExtensionCard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import styles from "./ExtensionCard.module.css";
 
 const ExtensionCard = ({ extension, isNightMode, onRemove }) => {
-  return isNightMode ? (
-    <div className={styles.card}>
+  return (
+    <div className={isNightMode ? styles.card : styles.cardLight}>
       <div className={styles.flex}>
         <img src={extension.logo} className={styles.logo} />
         <div>
@@ -18,33 +18,12 @@ const ExtensionCard = ({ extension, isNightMode, onRemove }) => {
           </button>
         </div>
         <label className={styles.switch}>
-          <input type="checkbox" checked={extension.isActive} />
+          <input type="checkbox" defaultChecked={extension.isActive} />
           <span className={styles.slider}></span>
         </label>
       </div>
     </div>
-  ) : (
-    <div className={styles.cardLight}>
-      <div className={styles.flex}>
-        <img src={extension.logo} className={styles.logo} />
-        <div>
-          <div className={styles.titleLight}>{extension.name}</div>
-          <div className={styles.bodyLight}>{extension.description}</div>
-        </div>
-      </div>
-      <div className={styles.flex}>
-        <div>
-          <button className={styles.buttonLight} onClick={onRemove}>
-            Remove
-          </button>
-        </div>
-        <label className={styles.switchLight}>
-          <input type="checkbox" checked={extension.isActive} />
-          <span className={styles.sliderLight}></span>
-        </label>
-      </div>
-    </div>
-  );
+  )
 };
 
 export default ExtensionCard;
