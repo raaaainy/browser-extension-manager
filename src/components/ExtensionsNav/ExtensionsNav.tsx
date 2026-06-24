c2433b79d03c8864f74bfffbae8122ce222dc266
Clarissa Barr <clarissab8788@gmail.com>
2026-06-24 15:43:33 -0700

    Converted Project to TypeScript

--- src/components/ExtensionsNav/ExtensionsNav.tsx
+++ src/components/ExtensionsNav/ExtensionsNav.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import styles from "./ExtensionsNav.module.css";
 
-const ExtensionsNav = ({ isNightMode }) => {
+interface ExtensionNavProps {
+  isNightMode: boolean;
+}
+
+const ExtensionsNav: React.FC<ExtensionNavProps> = ({ isNightMode }) => {
   return (
     <div className={isNightMode ? styles.navbar : styles.navbarLight}>
       <div className={styles.flex}>
         <img src={isNightMode ? `/images/logo-dark.png` : `/images/logo.svg`} />
-        <button className={isNightMode ? styles.nightModeButton : styles.lightModeButton}>
+        <button
+          className={
+            isNightMode ? styles.nightModeButton : styles.lightModeButton
+          }
+        >
           <img
             src={isNightMode ? `/images/icon-sun.svg` : `/images/icon-moon.svg`}
             className={styles.center}
